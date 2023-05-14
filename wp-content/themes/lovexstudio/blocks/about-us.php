5ff25aac13b03658a5c82e107c5020c1c376375d
Minh Thanh <vuxminhthanh@gmail.com>
2023-05-14 22:28:58 +0700

    Add home-about-us

---
+++ wp-content/themes/lovexstudio/blocks/about-us.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+
+<div class="home-about-us__top">
+    <div class="home-about-us__media">
+
+    </div>
+    <div class="home-about-us__desc">
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+    </div>
+</div>
+<div class="home-about-us__bottom">
+    <div class="about-us__row">
+        <div class="about-us__row-title">
+            Tittle 01
+        </div>
+        <div class="about-us__row-desc">
+            agna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie co
+        </div>
+        <div class="about-us__row-title">
+            Tittle 01
+        </div>
+        <div class="about-us__row-desc">
+            agna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie co
+        </div>
+        <div class="about-us__row-title">
+            Tittle 01
+        </div>
+        <div class="about-us__row-desc">
+            agna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie co
+        </div>
+    </div>
+</div>
