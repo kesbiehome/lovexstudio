2352d8527d00ce8650e6d83466c743083480cef7
Kesbie Ng <kesbie.nguyen@gmail.com>
2023-06-15 18:55:10 +0700

    Build and fix script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,7 @@ cd ./wp-content/themes/lovexstudio
 rm -rf assets/js/*.js.LICENSE.txt
 rm -rf src/
 rm -rf postcss.config.js
+rm -rf node_modules/
 rm -rf package*.json
 rm -rf .babelrc .browserslistrc .editorconfig .eslintrc.js .prettierignore .stylelintrc .gitignore
 rm -rf webpack.config.js CONTRIBUTING.md CHANGELOG.md composer.json
