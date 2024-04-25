8c6a53758be7ea75006cfbfcc828e0b364541249
kesbie <kesbie.nguyen@gmail.com>
2024-04-26 03:48:36 +0700

    feat: create cta and form section

--- wp-content/themes/lovexstudio/inc/helpers/shortcodes.php
+++ wp-content/themes/lovexstudio/inc/helpers/shortcodes.php
@@ -1,7 +1,8 @@
 <?php
 add_shortcode('social_links', 'social_links_callback');
 
-function social_links_callback() {
+function social_links_callback()
+{
 	$social_links = get_field('socials', 'options');
 	$content = '';
 
@@ -10,9 +11,39 @@ function social_links_callback() {
 		<div class="social-links">
 			<?php get_template_part('template-parts/social-links'); ?>
 		</div>
-	<?php
+		<?php
 	}
 	$content = ob_get_clean();
 
+	return $content;
+}
+
+
+add_shortcode('contact-form', 'contact_form_section_callback');
+function contact_form_section_callback()
+{
+	$image = get_field('form_image', 'options');
+
+	ob_start();
+	the_block('contact-form', [
+		'image' => $image,
+	]);
+	$content = ob_get_clean();
+
+	return $content;
+}
+
+add_shortcode('cta-section', 'cta_section_callback');
+
+function cta_section_callback()
+{
+
+	$image = get_field('cta_image', 'options');
+
+	ob_start();
+	the_block('cta', [
+		'image' => $image,
+	]);
+	$content = ob_get_clean();
 	return $content;
 }
